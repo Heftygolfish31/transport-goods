7234155ae02dfd6da237e685202aae15b405afe8
Heftygolfish31 <goldenblue@sundaydance.co.uk>
2021-05-25 20:53:53 +0100

    0.2.2
    See log/README.

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 Repo: transport-goods
-Ver: 0.2.1
+Ver: 0.2.2
 App: lesseasygui.py
 
 # NOTES #
 Version progression is Major, Minor, Patch.
 
 # LOG #
--0.0.1:
+- 0.0.1:
 	Repository created over python file for testing easyGui.
 	Made project checklisting.
 
--0.1.0:
+- 0.1.0:
 	Renamed lesseasygui.py to main.py.
 	Added log.txt.
 	Added credit function.
 	Temporarily disabled login function.
 
--0.2.0:
+- 0.2.0:
 	Added credit function.
 	Login still works.
 	Details are stored in endless (almost).
 	feildOut in main.py is far too cluncky but that's something to deal with later.
 
--0.2.1:
+- 0.2.1:
 	Added an invoice sheet.
 	Indented the invoice sheet.
 	Cried (only a little though).
 	You can't style easyGui!
+
+- 0.2.2:
+	Added comments.
+	Added IDEA type comment.
+	Labled code sub-sections.
+	Cleaner function (removing a function actually worked XD )
+	Edited README.md for readability.
+
+- 0.2.3 PRE:
--- main.py
+++ main.py
@@ -1,42 +1,54 @@
+# IMPORT #
 from easygui import *
 
 def login(password):
+    # Have you inputed the right password? (as specified in the function parameter)
     access = False
     while access == False:
         locked = passwordbox(msg="Authentication Required", title="Authenticate")
+        # If the input from the msg is equal to the correct password, proceed.
         if locked == password:
             print("Complete")
             access = True
+        # Else loop again.
         else:
             access = False
 
+
+# Handles card details and loops if you don't input the right details.
 def credit():
-    msg = "Enter credentials"
-    title = "Credentials"
-    fieldNames = ["Name", "Email", "Card Number", "Expration Date", "Security Code"]
-    fieldValues = list(multenterbox(msg, title, fieldNames))
-    
-    global fieldOut
-    name, email, card, expire, secure = fieldValues[0:len(fieldValues)]
-    fieldOut = [name, email, card, expire, secure]
-
-
-    print('\n'.join(fieldValues))
-
-def invoice():
     while True:
-        msg = f"INVOICE:\nBilled to:\t\t\t{fieldOut[0]}\nContact information:\t\t\t{fieldOut[1]}\nCard number:\t\t\t{fieldOut[2]}\n\t\t\t{fieldOut[3]}\n\t\t\t{fieldOut[4]}"
+        # IDEA: is having both variables named 'msg' a bad idea?
+        msg = "Enter credentials"
+        title = "Credentials"
+        fieldNames = ["Name", "Email", "Card Number", "Expration Date", "Security Code"]
+        fieldValues = list(multenterbox(msg, title, fieldNames))
+        
+        # Idea: What the fuqe is this?!
+        # Handels inputed values into discriptive variables.
+        global fieldOut
+        name, email, card, expire, secure = fieldValues[0:5]
+        fieldOut = [name, email, card, expire, secure]
+        # Non essential - DEBUG
+        print('\n'.join(fieldValues))
+
+        # Actual invoice display
+        # IDEA: link to .txt file if specs allow it.
+        msg = f"INVOICE:\n\nBilled to:\t\t\t{fieldOut[0]}\nContact information:\t\t\t{fieldOut[1]}\nCard number:\t\t\t{fieldOut[2]}\n\t\t\t{fieldOut[3]}\n\t\t\t{fieldOut[4]}"
         title = "Invoice"
         accurate = ynbox(msg, title)
+        
+        # If details verified by user, proceed.
         if accurate == True:
             break
+        # Else loop back again.
         else:
             continue
+        
 
-
+# RUN FUNCS #
 # remove for examination
 login("test")
 
 credit()
-invoice()
 
